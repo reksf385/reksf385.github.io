4a95f4ea4a5f415ee2460803a92b3ccf0e5e7846
Tata <reksf385@newschool.edu>
2018-10-17 21:44:26 -0400

    edit

--- t 3/assets/js/app.js
+++ t 3/assets/js/app.js
@@ -1,3 +1,11 @@
-var href={
-    
+var sites = [
+    'https://reksf385.github.io/',
+    'http://www.stackoverflow.com',
+    'http://www.example.com',
+    'http://www.youtube.com'
+];
+
+function randomSite() {
+    var i = parseInt(Math.random() * sites.length);
+    location.href = sites[i];
 }
